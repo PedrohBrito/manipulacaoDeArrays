cf21fb06a8e9e6669c44dcf1f4d409ea68341f64
pedro.h.cunha16@aluno.senai.br <pedro.h.cunha16@aluno.senai.br>
2025-05-30 16:02:22 -0300

    feat: Adiciona arquivo a pasta manipulacaoDeArrays

---
+++ questao02.js
@@ -0,0 +1,15 @@
+let cardapio = [
+  "pizza de calabresa",
+  "lasanha",
+  "sushi",
+  "salada Caesar",
+  "pastel",
+  "yakisoba",
+  "feijoada"
+];
+
+console.log(`Antes do push: ${cardapio}`);
+
+cardapio.push("hambúrguer artesanal", "wrap de frango");
+
+console.log(`Depois do push: ${cardapio}`);

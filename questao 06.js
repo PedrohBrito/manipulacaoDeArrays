1085fc211326373288a02c732e20e3f5fc5abbc6
pedro.h.cunha16@aluno.senai.br <pedro.h.cunha16@aluno.senai.br>
2025-05-30 16:14:41 -0300

    feat: Adiciona arquivo a pasta manipulacaoDeArrays

---
+++ questao 06.js
@@ -0,0 +1,11 @@
+let mensagens = [
+  "Propaganda indevida",
+  "Bom dia, posso ajudar?",
+  "Preciso de suporte técnico",
+  "Claro, qual o problema?",
+  "Erro ao acessar conta"
+];
+
+mensagens.shift();
+
+console.log(mensagens);

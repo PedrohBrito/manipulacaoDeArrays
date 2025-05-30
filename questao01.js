c71fa7b17ab29fe3dc9d553ddd7a6e99743b6107
pedro.h.cunha16@aluno.senai.br <pedro.h.cunha16@aluno.senai.br>
2025-05-30 15:54:56 -0300

    feat: Adiciona arquivo a pasta manipulacaoDeArrays

---
+++ questao01.js
@@ -0,0 +1,16 @@
+let promocoes = [
+  "teclado mecânico",
+  "mouse gamer",
+  "monitor ultrawide",
+  "cadeira ergonômica",
+  "webcam HD",
+  "fone bluetooth",
+  "notebook i5",
+  "hub USB"
+];
+
+console.log(`Antes do push: ${promocoes}`);
+
+promocoes.push("SSD 1TB", "mesa digitalizadora");
+
+console.log(`Depois do push: ${promocoes}`);

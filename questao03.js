dc3b66c6304014856c86578fd7bcfb2658d62f97
pedro.h.cunha16@aluno.senai.br <pedro.h.cunha16@aluno.senai.br>
2025-05-30 16:05:58 -0300

    feat: Adiciona arquivo a pasta manipulacaoDeArrays

---
+++ questao03.js
@@ -0,0 +1,13 @@
+let producao = [
+  "mousepad",
+  "placa de vídeo",
+  "fonte de alimentação",
+  "HD externo",
+  "notebook gamer",
+  "roteador"
+];
+
+
+producao.pop("hambúrguer artesanal", "wrap de frango");
+
+console.log(producao);

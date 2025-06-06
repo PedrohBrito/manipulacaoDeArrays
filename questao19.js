05fc93a24259d278e48869546482f63762188667
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-06-06 09:48:18 -0300

    feat: Adiciona o arquivo a pasta manipulacaoDeArrays

---
+++ questao19.js
@@ -0,0 +1,12 @@
+let codigos = [
+    "abc123",
+    "def456",
+    "ghi789",
+    "jkl012",
+    "mno345",
+    "pqr678",
+    "stu901",
+    "vwx234"
+  ];
+
+console.log(`${codigos.map(itens => itens.toUpperCase())}`);

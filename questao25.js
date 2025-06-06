63711c847b29e45d35754ea754e2d2e30547cdbd
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-06-06 11:42:09 -0300

    feat: Adiciona o arquivo a pasta manipulacaoDeArrays

---
+++ questao25.js
@@ -0,0 +1,12 @@
+let atualizacoes = [
+    "Backup diário concluído",
+    "Servidor reiniciado",
+    "Nova versão implantada",
+    "Logs exportados",
+    "Firewall atualizado",
+    "Monitoramento ativado",
+    "Banco de dados otimizado",
+    "Script de segurança executado"
+  ];
+
+console.log(`${atualizacoes.reverse()}`);
---
+++ questao26.js
@@ -0,0 +1,12 @@
+let historico = [
+    "login",
+    "painel",
+    "relatórios",
+    "detalhes do cliente",
+    "editar dados",
+    "configurações",
+    "logout"
+  ];
+
+  console.log(`${historico.reverse()}`);
+  

6d4ae359787840e237a8de1d2f604d740e7a2f9a
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-06-06 09:06:18 -0300

    feat: Adiciona o arquivo a pasta manipulacaoDeArrays

---
+++ questao10.js
@@ -0,0 +1,9 @@
+let estoque = [
+    "mesa para PC",
+    "cadeira gamer",
+    "monitor 27 polegadas",
+    "hub USB",
+    "mouse sem fio"
+  ];
+
+console.log(`${estoque.includes("cadeira gamer")}`);

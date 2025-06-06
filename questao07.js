f97fcfe47ff9922a09d558a23c9b00b8903a4980
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-06-06 08:56:20 -0300

    feat: Adiciona o arquivo a pasta manipulacaoDeArrays

---
+++ questao07.js
@@ -0,0 +1,13 @@
+let filaAtendimento = [
+    "cliente 003",
+    "cliente 004",
+    "cliente 005",
+    "cliente 006",
+    "cliente 007"
+  ];
+
+  
+filaAtendimento.unshift("cliente 002");
+filaAtendimento.unshift("cliente 001");
+
+console.log(filaAtendimento);

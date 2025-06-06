2dffb4c4f41526b95e4b2198ea48439001512060
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-06-06 11:07:05 -0300

    feat: Adiciona o arquivo a pasta manipulacaoDeArrays

---
+++ questao20.js
@@ -0,0 +1,12 @@
+let precos = [
+    129.9,
+    399.5,
+    259.0,
+    89.9,
+    599.99,
+    39.5,
+    999.0
+  ];
+
+let precosFormatados = precos.map(p => `R$ ${p.toFixed(2)}`);
+console.log(`Como texto: ${precosFormatados.join(', ')}`);

dce163b55e8520f90e9eb4e1f75e94413a4d7f9a
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-06-06 09:04:36 -0300

    feat: Adiciona o arquivo a pasta manipulacaoDeArrays

---
+++ questao09.js
@@ -0,0 +1,12 @@
+let presenca = [
+    "João Lima",
+    "Carla Nunes",
+    "Amanda Souza",
+    "Bruno Ribeiro",
+    "Fernanda Sales"
+  ];
+
+  
+
+console.log(`${presenca.includes("Amanda Souza")}`);
+

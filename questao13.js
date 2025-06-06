9255e1f24c40d6a145b4f10933cff9453fec2053
Pedro <pedro.h.cunha16@aluno.senai.br>
2025-06-06 09:16:39 -0300

    feat: Adiciona o arquivo a pasta manipulacaoDeArrays

---
+++ questao13.js
@@ -0,0 +1,12 @@
+let diasFaltas = [
+    "Carlos – Segunda",
+    "Carlos – Terça",
+    "Carlos – Quarta",
+    "Carlos – Quinta",
+    "Carlos – Sexta",
+    "Carlos – Segunda (semana 2)",
+    "Carlos – Terça (semana 2)",
+    "Carlos – Quarta (semana 2)"
+  ];
+
+console.log(`${diasFaltas.slice(0, 1)}`);
